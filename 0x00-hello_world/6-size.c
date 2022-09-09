887c785a1717babce6b04ba3e3fe965450a9bb13
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-09 22:21:04 +0100

    c code size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-
 	printf("size of a char: %i byte(s)\n", sizeof(char));
 	printf("size of an int: %i byte(s)\n", sizeof(int));
 	printf("size of a long int: %i byte(s)\n", sizeof(long int));
