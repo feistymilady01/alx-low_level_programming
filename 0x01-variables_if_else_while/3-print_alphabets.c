66b15d8b834c49a8d1e368cbc961ae0c7bd91d7d
Susan <pricelessblinks@gmail.com>
2022-09-09 19:59:39 -0700

    betty spacing

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 /**
  * main -print the alphabetic
- * 
+ *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
 
-	for (c='a'; c<= 'z'; c++)
-	
+	for ( c = 'a'; c <= 'z'; c++)
+
 	{
 		putchar(c);
 	}
 
-	for (c='A'; c<= 'Z'; c++)
+	for ( c = 'A'; c <= 'Z'; c++)
 
-        {
-                putchar(c);
-        }
-	
+	{
+		putchar(c);
+	}
+
 	putchar('\n');
-	
+
 	return(0);
 }
