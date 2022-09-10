d8a21be441910c30ae33f871a73be4269d0ecfcd
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-10 05:01:25 +0100

    three combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 /**
- * main -print putchar
+ * main -print three combination
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c, i;
+	int i, c, e;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (c = '0'; c <= '9'; c++)
 		{
-			if (c <i)
+			for (e = '0'; e <= '9'; e++)
 			{
-			putchar(c);
-			putchar(i);
-				if (c != '8' && i != '9')
+				if (c <i)
 				{
-					putchar(',');
-					putchar(',');
+					putchar(c);
+					putchar(i);
+					if (i != '7' || (c != '8' && e == '9'))
+					{
+						putchar(',');
+						putchar(',');
+					}
 				}
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
-
 }
