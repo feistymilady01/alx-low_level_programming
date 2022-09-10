8be5d2e0d57c17c69043fe58036ea9233567b843
Susan <pricelessblinks@gmail.com>
2022-09-09 19:24:29 -0700

    print 0_10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -print the numbers
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putcahar('\n')
+
+	return (0);
+}
