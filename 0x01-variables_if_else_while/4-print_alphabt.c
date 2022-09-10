64d303f55f622c379c00ca2c81d71a9ba08fbb38
Susan <pricelessblinks@gmail.com>
2022-09-09 19:44:59 -0700

    numbers in c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char c;
 
-	for ( c = 'a'; c <= 'z'; c++)
-	
+	for (c = 'a'; c <= 'z'; c++)
+
 	{
-		if ( c != 'e' && c != 'q')
+		if (c != 'e' && c != 'q')
 		{
 			putchar(c);
 		}
