a5518a9c76ee9f2581c9e7857c71f2a96c3c810e
Susan <pricelessblinks@gmail.com>
2022-09-09 20:21:12 -0700

    betty space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -print putchar
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+
+		if (c != '9' )
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}putchar('\n');
+	return (0);
