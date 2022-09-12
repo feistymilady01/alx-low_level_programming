eb0b2f37bd8349edd8411382fb7717c6536cb22f
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-13 00:26:38 +0100

    data size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,18 @@
  */
 int main(void)
 {
-	printf("size of a char: %li byte(s)\n", sizeof(char));
-	printf("size of an int: %li byte(s)\n", sizeof(int));
-	printf("size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %li byte(s)\n", sizeof(long long int));
-	printf("size of a float: %li byte(s)\n", sizeof(float));
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	
+	
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
