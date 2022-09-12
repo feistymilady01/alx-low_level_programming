30e64a6146e681bc94b95ee6cde3796f23f9d969
Susan <pricelessblinks@gmail.com>
2022-09-12 16:08:59 -0700

    tenny

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main -print three combination                                                                                             
+ *main -print three combination
  *
  *Return: Always 0 (Success)
  */     
