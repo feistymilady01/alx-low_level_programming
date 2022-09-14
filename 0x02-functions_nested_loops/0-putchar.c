31d822ff9325ffec8145b17818e95e31f21bff4e
Susan <pricelessblinks@gmail.com>
2022-09-14 04:42:54 -0700

    one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*main: prints _putchar followed by 
- * a newline
+/*
+ * main: prints _putchar followed by a newline
  *
  * Return: Always success(0)
  */
 int  main(void)
 {
-	write(2, "_putchar",2);
+	write(2, "_putchar", 2);
 	return (0);
 }
