adfbf28768e57d5d5a83505812e47c163de4bb2c
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-15 04:13:08 +0100

    fibona

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@ void printFibonacciNumbers(int n)
     printf("%d ", f1);
     for (i = 1; i < n; i++) {
         printf("%d ", f2);
-        int next = f1 + f2;
+        next = f1 + f2;
         f1 = f2;
         f2 = next;
     }
