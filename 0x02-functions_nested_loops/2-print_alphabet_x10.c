a568146b950a18f27f3cade3b2be5f91b8265156
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-15 03:29:03 +0100

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
  */
 void print_alphabet_x10(void)
 {
-    int n, co;
-    
-    co = 0;
+	int n, co;
 
-    while (co < 10)
-    {
-          for (n = 'a'; n <= 'z'; n++)
-          {
-                  _putchar(n);
-          }
-          co++;
-          _putchar('\n');
-    }
+	co = 0;
+
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
 }
