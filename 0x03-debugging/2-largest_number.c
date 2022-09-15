790d5e0858922e791d0f125680b8968a38743b1d
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-15 21:06:53 +0100

    return largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,18 +9,14 @@
  */
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && b > c)
-{
-largest = a;
-}
-else if (b > a && a > c)
-{
-largest = b;
-}
-else
-{				
-largest = c;
-}
-return (largest);
+	int largest = a;
+
+	if (largest < b)
+	{
+		largest = b;
+		if (largest < c)
+			largest = c;
+	}
+
+	return (largest);
 }
