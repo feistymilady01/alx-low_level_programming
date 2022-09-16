ad0030e91c6272e7a0a6737e132db146784ba3f5
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-16 02:24:22 +0100

    upper cas

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
- * Return: Always 0 (Succes)
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		return (1);
-	}
-
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
