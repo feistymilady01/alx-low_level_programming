85c99285541a8ff07f13878b06a4c8aeab39dedc
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-16 02:42:54 +0100

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,7 +8,7 @@ void print_most_numbers(void);
 int _isupper(int);
 int _isdigit(int);
 void print_line(int n);
-int print_diagonal(int n);
+void print_diagonal(int n);
 int print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
