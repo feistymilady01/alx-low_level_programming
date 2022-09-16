9edf2c5aa783b746ceda7fac6d807f431bb7cc0f
Susan <pricelessblinks@gmail.com>
2022-09-15 17:39:20 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ * Return: Always 0 (Succes)
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char);
+void print_numbers(void);
+void more_numbers(void);
+void print_most_numbers(void);
+int _isupper(int);
+int _isdigit(int);
+int print_line(int n);
+int print_diagonal(int n);
+int print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int mul(int, int);
+
+#endif /*MAIN_H*/
