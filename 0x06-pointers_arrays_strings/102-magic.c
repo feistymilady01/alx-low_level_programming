92a6a9f438b151073a8a47c504f36392321b4f9a
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-09-22 14:44:53 +0100

    hope it works

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 /**
- * rot13 - encodes magic
- * @s: string to be encoded
- *
- * Return: the resulting string
+ * encoding
  */
 int main(void)
 {
@@ -21,7 +18,7 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-(p + 5) = 98;
+*(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
