d0f966e4c5ea7bb3a86e5113dee4478e83b36cb5
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-10-27 19:48:08 +0100

    update of binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,37 @@
 #include "main.h"
+
 /**
-  * binary_to_uint - converts a binary number to an unsigned int
-  * @b: binary number
-  * Return: the converted number or 0 if it fails
-  */
+ * binary_to_uint - converts a binary number to an
+ * unsigned int
+ * @b: binary
+ *
+ * Return: unsigned int
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int uint, place;
-	int n;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	for (n = 0; b[n]; n++)
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
 		;
-	n--;
-	for (place = 1, uint = 0; n >= 0; n--)
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[n] == '0')
+		if (b[len] != '0' && b[len] != '1')
 		{
-			place *= 2;
-			continue;
+			return (0);
 		}
-		else if (b[n] == '1')
+
+		if (b[len] & 1)
 		{
-			uint += place;
-			place *= 2;
-			continue;
+			ui += base_two;
 		}
-		return (0);
 	}
-	return (uint);
+	return (ui);
 }
