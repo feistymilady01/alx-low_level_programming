fe711481e780dc47f302d58c04dca3d911afa1d1
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-12-13 12:04:26 +0100

    update

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,1 +1,17 @@
-d,nvsnv,s v
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
