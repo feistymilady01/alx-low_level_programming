8d31cc493cc15b799a9a9de72fa365ef01f279cc
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-01-09 22:52:30 +0100

    key tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,20 @@
-jhdj
+#include "hash_tables.h"
+
+/**
+ * key_index - Retrieves the index of a key
+ *
+ * @key: The key
+ * @size: Size of array in the hash table
+ *
+ * Return: The index at which the key should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index, hash;
+
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return (index);
+}
